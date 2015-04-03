652e7d6e96c92a88e33fa78772592c36097acda0
Zephilinox <Zephilinox@hotmail.co.uk>
2015-04-03 20:18:42 +0100

    WRECK options

--- module_info.py
+++ module_info.py
@@ -1,7 +1,29 @@
-
-# Point export_dir to the folder you will be keeping your module
-# Make sure you use forward slashes (/) and NOT backward slashes (\)
-
 export_dir = "Bean/"
-#export_dir = "../WOTS/Modules/Native/"
-#export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
+
+###################################
+#   W.R.E.C.K. Compiler Options   #
+###################################
+
+
+# Change this line to select where compiler will generate ID_* files. Use None instead of the string to completely suppress generation of ID_* files.
+# ONLY DO THIS WHEN YOU HAVE COMPLETELY REMOVED ID_* FILE DEPENDENCIES IN MODULE SYSTEM!
+# Default value: "ID_%s.py"
+
+#write_id_files = "ID_%s.py"    # default vanilla-compatible option
+#write_id_files = "ID/ID_%s.py" # will put ID_* files in ID/ subfolder of module system's folder
+write_id_files = None          # will suppress generation of ID_*.py files
+
+
+# Set to True to display compiler performance information at the end of compilation. Set to False to suppress.
+# Default value: False
+
+show_performance_data = False
+
+
+
+##########################
+#   W.R.E.C.K. Plugins   #
+##########################
+
+#import plugin_ms_extension
+#import plugin_presentations
