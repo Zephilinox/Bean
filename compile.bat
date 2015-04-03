4adca1546d30e61f019b0cc66a56b9fb03574938
Zephilinox <Zephilinox@hotmail.co.uk>
2015-04-03 19:54:52 +0100

    Bean MS 1.166 with WRECK

---
+++ compile.bat
@@ -0,0 +1,5 @@
+@echo off
+SET PATH=%PATH%;C:\Python27
+python compile.py tag %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
+
---
+++ module_info.py
@@ -0,0 +1,7 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+export_dir = "Bean/"
+#export_dir = "../WOTS/Modules/Native/"
+#export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
